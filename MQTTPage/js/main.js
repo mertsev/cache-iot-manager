92ac92f0c25a226e97c04372dd529bc6d046609c
tairendark <tairendark@mail.ru>
2016-07-24 23:25:29 +0700

    New version GUI

--- MQTTPage/js/main.js
+++ MQTTPage/js/main.js
@@ -6,56 +6,129 @@
 });
 
 
+
 // getting data source
-var app = angular.module('testApp', []);
-app.controller('testCtrl', function($scope, $http) {
+var app = angular.module('MQTTApp', []);
+app.controller('MQTTCtrl', function($scope, $http) {
   $scope.isConnected = false;
-  $scope.connectClass = "btn-success";
-  $scope.connectText = "Connect";
-  $scope.subscribeClass = "btn btn-block btn-primary";
-  $scope.subscribeText = "Subscribe";
-  $scope.connOrDisc = function () {
-    if (!$scope.isConnected)
-    {
-      $scope.getData();
+  $scope.ShowNotice = function(nHead, nText, nType) {
+    if (nHead == undefined) {
+      nHead = "Title";
     }
-    else
-    {
-      $scope.Disconnect();
-      $scope.connectClass = "btn-success";
-      $scope.connectText = "Connect";
-      $scope.isConnected = false;
+    if (nText == undefined) {
+      nText = "Text";
     }
-  }
+    if (nType == undefined) {
+      nType = 1;
+    }
+    if (nType == 3) {
+      $scope.modalType = "modal-error";
+    } else if (nType == 2) {
+      $scope.modalType = "modal-warning";
+    } else {
+      $scope.modalType = "modal-info";
+    }
+    $scope.noticeHeader = nHead;
+    $scope.noticeText = nText;
+    $('#NoticeModal').modal('show');
+  };
   // Connect
-  $scope.getData = function() {
+  $scope.Connect = function() {
     $http({
       method: 'GET',
       url: "../../rest/json/connect/" + $scope.UserId
     }).success(function(data, status, header, config) {
-	  $scope.connectClass = "btn-danger";
-      $scope.connectText = "Disconnect";
+      if (!data.success) {
+        $scope.ErrMessages = data.error;
+        return false;
+      }
       $scope.isConnected = true;
       $scope.ClientObj = data;
+      var $winArea = $('#window-area');
+      $winArea.delay(100).fadeOut('slow');
     }).error(function(data, status, header, config) {
-      alert("incorrect url!");
+      $scope.ErrMessages = "Connection error!";
   });
   }
   // Disconnect
   $scope.Disconnect = function() {
-    var act = "../../rest/json/disconnect/" + $scope.UserId;
-    $http.get(act);
+    $http({
+      method: 'GET',
+      url: "../../rest/json/disconnect/" + $scope.UserId
+    }).success(function(data, status, header, config) {
+      if (!data.success) {
+        $scope.ShowNotice('Disconnectoin error', data.error, 3);
+        return false;
+      }
+      $scope.isConnected = false;
+      var $winArea = $('#window-area');
+      $winArea.delay(100).fadeIn('slow');
+    }).error(function(data, status, header, config) {
+      $scope.ShowNotice('Disconnection error', data.error, 3);
+    });
   }
   // Subscribe
   $scope.Subscribe = function() {
+    // Getting array of topics
+    var topics = $scope.link.split(", ");
+    var dataObjs = new Array();
+    for (var i=0; i<topics.length; i++) {
+      dataObjs.push({"topicFilter": topics[i], "qos": "2"})
+    }
     $http({
       method: 'POST',
+      headers: "",//{ 'Content-Type': 'application/json' },
       url: "../../rest/json/subscribe/" + $scope.ClientObj.clientObject,
-      data:   { "topicFilter": $scope.link, "qos": 2 }
+      data: dataObjs
     }).success(function(data, status, header, config) {
-      $scope.myData = data.children;
+      if (!data.success) {
+        $scope.ShowNotice('Subscribe error', data.error, 3);
+        return false;
+      }
+      var bufstr = data.topics[0].topicFilter;
+      for (var i=1; i<data.topics.length; i++) {
+        bufstr += ", " + data.topics[i].topicFilter;
+      }
+      $scope.ShowNotice('Subscribe', 'Subscribe successfully complete! \nYou are subscribed to: ' + bufstr + ".");
     }).error(function(data, status, header, config) {
-      alert("obj not found!");
+      $scope.ShowNotice('Subscribe error', data.error, 3);
+    });
+  }
+
+  // Unubscribe
+  $scope.Unsubscribe = function() {
+    // Getting array of topics
+    var topics = $scope.link.split(", ");
+    var dataObjs = new Array();
+    for (var i=0; i<topics.length; i++) {
+      dataObjs.push({"topicFilter": topics[i], "qos": "2"})
+    }
+    $http({
+      method: 'POST',
+      headers: "",//{ 'Content-Type': 'application/json' },
+      url: "../../rest/json/unsubscribe/" + $scope.ClientObj.clientObject,
+      data: dataObjs
+    }).success(function(data, status, header, config) {
+      if (!data.success) {
+        $scope.ShowNotice('Unsubscribe error', data.error, 3);
+        return false;
+      }
+      $scope.ShowNotice('Subscribe', 'Unubscribe successfully complete! \nYou are unsubscribed to: ' + $scope.link + ".");
+    }).error(function(data, status, header, config) {
+      $scope.ShowNotice('Unsubscribe error', data.error, 3);
+    });
+  }
+
+  $scope.GetMessages = function() {
+    $http({
+      method: 'GET',
+      url: "mes2.html"
+    }).success(function(data, status, header, config) {
+      $scope.myData = data;
+      $scope.ShowNotice('Data', 'The data was successfully loaded.');
+    }).error(function(data, status, header, config) {
+      $scope.ShowNotice('Data', 'Error loading data.', 3);
   });
   }
+
 });
